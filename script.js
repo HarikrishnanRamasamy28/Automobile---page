e58cb5b26aacd1a6f934f88756179a794c2136a4
HarikrishnanRamasamy28 <krish96882@gmail.com>
2025-11-19 13:53:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,4 +12,39 @@ hamburger.addEventListener("click", () => {
 
 
 
+// document.addEventListener("DOMContentLoaded", function() {
+//   const observer = new IntersectionObserver((entries, obs) => {
+//     entries.forEach(entry => {
+//       console.log("Entry:", entry, "isIntersecting:", entry.isIntersecting);
 
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("in-view");
+//         obs.unobserve(entry.target);
+//       }
+//     });
+//   }, { threshold: 0.2 });
+
+//   // Observe all elements that should animate when scrolled into view
+//   document.querySelectorAll(".animate-on-scroll").forEach(el => {
+//     observer.observe(el);
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+// buying guide js code 
+// JavaScript to handle toggle
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    const item = button.parentElement;
+    item.classList.toggle('open');
+  });
+});
